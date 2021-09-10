5b95082034d7d4aafb4d5b245e996dbf630be2ac
Longas13 <g.pacevicius@gmail.com>
2021-09-10 11:48:38 +0300

    THEME - add songs + uuid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import Player from "./conponents/Player";
 import Song from "./conponents/Song";
-import "./styles/app.scss"
+import "./styles/app.scss";
+import data from "./util";
 
 function App() {
+
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
